26b46c943dbcdcbf8912488fd3fbc35847ff8705
Shehab <shehabhafez3@gmail.com>
2020-10-11 15:18:15 +0200

    fixed

--- src/com/github/stoneskies/FeaturesMain.java
+++ src/com/github/stoneskies/FeaturesMain.java
@@ -4,11 +4,11 @@ import com.github.stoneskies.VillagerPerms.VPMain;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FeaturesMain extends JavaPlugin {
-
+    @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(new VPMain(), this);
     }
-
+    @Override
     public void onDisable() {
 
     }
--- src/com/github/stoneskies/VillagerPerms/VPMain.java
+++ src/com/github/stoneskies/VillagerPerms/VPMain.java
@@ -2,22 +2,23 @@ package com.github.stoneskies.VillagerPerms;
 
 import com.palmergames.bukkit.towny.object.TownyPermission;
 import com.palmergames.bukkit.towny.utils.PlayerCacheUtil;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class VPMain implements Listener {
 
+    @EventHandler
     public void onVillagerInteract(PlayerInteractEntityEvent event) {
-        Entity entity = event.getRightClicked();
         Player player = event.getPlayer();
-        Block block = player.getLocation().getBlock();
-        if (entity.getType() == EntityType.VILLAGER) {
-            boolean bBuild = PlayerCacheUtil.getCachePermission(player, player.getLocation(), block.getType(), TownyPermission.ActionType.ITEM_USE);
-            if (!bBuild) {
+        if (event.getRightClicked() instanceof Villager) {
+            boolean hasperms = PlayerCacheUtil.getCachePermission(player, player.getLocation(), Material.CHEST, TownyPermission.ActionType.SWITCH);
+            if (!hasperms) {
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6You don't have permissions to trade with villagers here."));
                 event.setCancelled(true);
             }
         }
