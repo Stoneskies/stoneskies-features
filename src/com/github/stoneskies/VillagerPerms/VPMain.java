dd1d84390d2f5e004b2246ea57c12c952adf1a8f
Shehab <shehabhafez3@gmail.com>
2020-10-12 22:02:05 +0200

    berry bushes

--- src/com/github/stoneskies/VillagerPerms/VPMain.java
+++ src/com/github/stoneskies/VillagerPerms/VPMain.java
@@ -4,10 +4,12 @@ import com.palmergames.bukkit.towny.object.TownyPermission;
 import com.palmergames.bukkit.towny.utils.PlayerCacheUtil;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerHarvestBlockEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 public class VPMain implements Listener {
@@ -19,6 +21,17 @@ public class VPMain implements Listener {
             boolean hasperms = PlayerCacheUtil.getCachePermission(player, player.getLocation(), Material.CHEST, TownyPermission.ActionType.SWITCH);
             if (!hasperms) {
                 player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6You don't have permissions to trade with villagers here."));
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBerryBush(PlayerHarvestBlockEvent event) {
+        Player player = event.getPlayer();
+        boolean hasperms = PlayerCacheUtil.getCachePermission(player, player.getLocation(), Material.CHEST, TownyPermission.ActionType.SWITCH);
+        if(event.getHarvestedBlock().getType() == Material.SWEET_BERRY_BUSH) {
+            if(!hasperms) {
                 event.setCancelled(true);
             }
         }
